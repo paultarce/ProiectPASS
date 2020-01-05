6931ff439625ccdcb681b91cb58cdfea90e71316
paultarce <32434382+paultarce@users.noreply.github.com>
2020-01-05 15:27:55 +0200

    05.01 Icon Added

--- ProiectAndroid/app/src/main/java/com/example/alarmreminder/AddReminderActivity.java
+++ ProiectAndroid/app/src/main/java/com/example/alarmreminder/AddReminderActivity.java
@@ -9,6 +9,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
+import android.graphics.drawable.Icon;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
